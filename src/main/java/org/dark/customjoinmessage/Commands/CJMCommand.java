2be8cc30d1f4a1fb88511ecdad3bc4ffc5cd70eb
Sinkazo <sincoalfredo@gmail.com>
2024-09-09 10:12:18 -0600

    This code allows players to set a customized server join message

--- src/main/java/org/dark/customjoinmessage/Commands/CJMCommand.java
+++ src/main/java/org/dark/customjoinmessage/Commands/CJMCommand.java
@@ -32,16 +32,16 @@ public class CJMCommand implements CommandExecutor {
         if (args.length == 1 && args[0].equalsIgnoreCase("setjoinmessage")) {
             plugin.setPlayerConfiguring(player.getUniqueId(), true);
             plugin.setConfiguringJoinMessage(player.getUniqueId(), true);
-            player.sendTitle("Configuración", "Escribe tu mensaje de conexión en el chat", 10, 70, 20);
-            player.sendMessage(ChatColor.GOLD + "Ahora estás en el modo de configuración de tu mensaje de conexión, escribe " + ChatColor.RED + "cancel" + ChatColor.GOLD + " para salir.");
+            player.sendTitle("Configuration", "Write your connection message in the chat", 10, 70, 20);
+            player.sendMessage(ChatColor.GOLD + "Now you are in the configuration mode. Type" + ChatColor.RED + " cancel " + ChatColor.GOLD + "to exit.");
             return true;
         }
 
         if (args.length == 1 && args[0].equalsIgnoreCase("setquitmessage")) {
             plugin.setPlayerConfiguring(player.getUniqueId(), true);
             plugin.setConfiguringJoinMessage(player.getUniqueId(), false);
-            player.sendTitle("Configuración", "Escribe tu mensaje de desconexión en el chat", 10, 70, 20);
-            player.sendMessage(ChatColor.GOLD + "Ahora estás en el modo de configuración de tu mensaje de desconexión, escribe " + ChatColor.RED + "cancel" + ChatColor.GOLD + " para salir.");
+            player.sendTitle("Configuration", "Write your disconnection message in chat", 10, 70, 20);
+            player.sendMessage(ChatColor.GOLD + "Now you are in the configuration mode. Type" + ChatColor.RED + " cancel " + ChatColor.GOLD + "to exit.");
             return true;
         }
 
